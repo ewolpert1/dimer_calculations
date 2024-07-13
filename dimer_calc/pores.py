e988a1157ff0752bfed01c9c7ca970c7558775dc
andrewtarzia <andrew.tarzia@gmail.com>
2024-07-13 09:22:53 +0200

    Clean up and add com distance test, much faster.

--- dimer_calc/pores.py
+++ dimer_calc/pores.py
@@ -1,14 +1,18 @@
 """Pores module."""
 
+import numpy as np
 import pore_mapper as pm
+import stk
+import stko
+from scipy.spatial.distance import cdist
 
 
 def two_pores(
-    # write_name,
-    stk_molecule,
-    centroid1,
-    centroid2,
-):
+    stk_molecule: stk.Molecule,
+    centroid1: np.ndarray,
+    centroid2: np.ndarray,
+) -> list[float]:
+    """Run poremapper on dimer."""
     # Read in host from xyz file.
     host = pm.Host(
         atoms=(
@@ -17,9 +21,6 @@ def two_pores(
         ),
         position_matrix=stk_molecule.get_position_matrix(),
     )
-
-    # hosts = (host1, host2)
-    # centroids = (centroid1, centroid2)
 
     # Number of centroids can be any number:
     pore_distances = []
@@ -35,11 +36,10 @@ def two_pores(
         pore_distances.append(pore.get_mean_distance_to_com())
 
     return pore_distances
-    # assert np.isclose(pore_distances[0], pore_distance1, atol=1e-6, rtol=0)
-    # assert np.isclose(pore_distances[1], pore_distance2, atol=1e-6, rtol=0)
 
 
-def one_pore(stk_molecule):
+def one_pore(stk_molecule: stk.Molecule) -> float:
+    """Run poremapper on one cage."""
     # Read in host from xyz file.
     host = pm.Host(
         atoms=(
@@ -49,34 +49,50 @@ def one_pore(stk_molecule):
         position_matrix=stk_molecule.get_position_matrix(),
     )
 
-    # Number of centroids can be any number:
-    pore_distances = []
     # Define calculator object.
     calculator = pm.Inflater(bead_sigma=1.2, centroid=host.get_centroid())
 
     final_result = calculator.get_inflated_blob(host=host)
     pore = final_result.pore
-    pore_distances.append(pore.get_mean_distance_to_com())
+
+    return pore.get_mean_distance_to_com()
+
+
+def two_com_distances(stk_molecule: stk.Molecule) -> list[float]:
+    """Get com distances on dimer."""
+    graph = stko.Network.init_from_molecule(stk_molecule)
+
+    # A series of graphs still connected.
+    connected_graphs = graph.get_connected_components()
+
+    pore_distances = []
+    for cg in connected_graphs:
+        # Get atoms from nodes.
+        atoms = list(cg)
+        atom_ids = tuple(i.get_id() for i in atoms)
+
+        pair_dists = cdist(
+            stk_molecule.get_position_matrix(),
+            stk_molecule.get_centroid(atom_ids).reshape(1, 3),
+        )
+        pore_distances.append(np.min(pair_dists.flatten()))
 
     return pore_distances
 
 
-def check_catenane(one_pore, two_pore, cutoff=0.2):
-    # print(one_pore,two_pore)
-    if two_pore[0] < (one_pore[0] - cutoff) and two_pore[1] < (
-        one_pore[0] - cutoff
-    ):
-        catenane = True
-    else:
-        catenane = False
-    return catenane
+def one_com_distance(stk_molecule: stk.Molecule) -> float:
+    """Get com distance on one cage."""
+    return stko.molecule_analysis.GeometryAnalyser().get_min_centroid_distance(
+        stk_molecule
+    )
 
 
-# cage_name='G_17'
-# dimer_name="Cage_G_17_63_6_aa"
-# cage_dimer=stk.BuildingBlock.init_from_file(f"{dimer_name}.mol")
-# cage=stk.BuildingBlock.init_from_file(f"{cage_name}.mol")
-#
-# cage_pore=one_pore(cage_name,cage)
-# dimer_pores=np.min(two_pores(dimer_name,cage_dimer,np.array((0, 0, 0)),np.array((0.41359275, -4.41492435, 12.58085734))))
-# print(dimer_pores)
+def check_catenane(
+    one_pore: float,
+    two_pore: list[float],
+    cutoff: float = 0.2,
+) -> bool:
+    """Check if two cages are catenated."""
+    return bool(
+        two_pore[0] < (one_pore - cutoff) and two_pore[1] < (one_pore - cutoff)
+    )
