8d7a5c7126da2e5380b17f8d3ce3f7a1b81aca60
ewolpert1 <86250984+ewolpert1@users.noreply.github.com>
2024-08-12 10:21:36 +0100

    Update Optimiser_functions.py

--- dimer_calculations/Optimiser_functions.py
+++ dimer_calculations/Optimiser_functions.py
@@ -640,7 +640,7 @@ class DimerOptimizer:
         structure = OPLS_opt.optimize(mol=dimer, fixed_atom_set=fixed_atom_set)
         structure.write(f'{output_dir}_opt.mol')
 
-    def optimise_dimer_XTB(dimer, output_dir, XTB_PATH,opt_level='normal',num_cores=1,electronic_temperature=300, solvent_model='gbsa', solvent=None, solvent_grid='normal',charge=0,unpaired_electrons=0, fixed_atom_set=None,unlimited_memory=False):
+    def optimise_dimer_XTB(dimer, output_dir, XTB_PATH,opt_level='normal',num_cores=1,electronic_temperature=300, solvent_model='gbsa', solvent=None, solvent_grid='normal',charge=0,unpaired_electrons=0,calculate_hessian=False, fixed_atom_set=None,unlimited_memory=False):
         if not os.path.exists(output_dir):
             os.makedirs(output_dir)
         
@@ -658,7 +658,7 @@ class DimerOptimizer:
                 charge=charge,
                 num_cores=num_cores,
                 num_unpaired_electrons=unpaired_electrons,
-                calculate_hessian=True,
+                calculate_hessian=calculate_hessian,
                 fixed_atom_set=fixed_atom_set,
             )
             structure = XTB_opt.optimize(mol=dimer, fixed_atom_set=fixed_atom_set)
