b35faf675654d1311eece0491e7e9afbc498be01
andrewtarzia <andrew.tarzia@gmail.com>
2024-09-02 16:55:28 +0200

    Clean up.

--- scripts/test.py
+++ scripts/test.py
@@ -18,13 +18,14 @@ def main() -> None:
     molecule = stk.BuildingBlock.init_from_file(filename)
 
     """
-    This function returns the vectors between the centroid of the molecule and the
-    smiles string and the magnitude/distance.
+    This function returns the vectors between the centroid of the molecule and
+    the smiles string and the magnitude/distance.
 
     You need to give it:
         molecule: stk.Molecule
-        smiles_string: str. Here there is a remove_aldehyde function you can use
-        which takes away the aldehydes so you can use the precursor string exactly
+        smiles_string: str. Here there is a remove_aldehyde function you can
+        use which takes away the aldehydes so you can use the precursor string
+        exactly
 
     """
 
@@ -34,16 +35,16 @@ def main() -> None:
     )
 
     """
-    You can then take these vectors and use them to determine the centroids of the
-    facets, even if theyre windows.
+    You can then take these vectors and use them to determine the centroids of
+    the facets, even if theyre windows.
 
     For this you need to give it:
         molecule: stk.Molecule
         vectors: list of vertices
         vertice_size: float
-        no_vectors_define_facet: int. This is the number of vectors that define a
-        facet. For example for a 6+4 cage it is 3 but for truncated octahedron it
-        could be 6
+        no_vectors_define_facet: int. This is the number of vectors that define
+        a facet. For example for a 6+4 cage it is 3 but for truncated
+        octahedron it could be 6
         tolerance: float. As this function measures the distance between the
         vertices to get the ones forming a facet, this is the tolerance for
         similar the distances have to be where 0.1 is 10% (I think).
@@ -78,17 +79,19 @@ def main() -> None:
         molecule: stk.Molecule
         axes: The axis you want to displace along from the orginal cage
         second_cage_orientation: The orientation of the displaced cage. This
-        funciton rotates the molecule so the vector you give is facing the original
-        cage. For window to arene you just need it the same, but for
-        window-to-window or window-to-arene the cage must be inverted, hence why
-        it is -axis.
+        funciton rotates the molecule so the vector you give is facing the
+        original cage. For window to arene you just need it the same, but for
+        window-to-window or window-to-arene the cage must be inverted, hence
+        why it is -axis.
         displacement_distance: float. This is the starting distance of the
         displacement between the two molecules. (It actually starts 2 angstroms
         closer)
-        displacement: float. This+displacement_distance is the maximum distance of
-        displacement
-        displacement_step_size: float. This is the step size of the displacement.
-        rotation_limit: float. This is the maximum rotation of the displaced cage.
+        displacement: float. This+displacement_distance is the maximum distance
+        of displacement
+        displacement_step_size: float. This is the step size of the
+        displacement.
+        rotation_limit: float. This is the maximum rotation of the displaced
+        cage.
         rotation_step_size: float. This is the step size of the rotation.
         slide: bool. This is whether you want translating ove the face or not.
 
@@ -97,8 +100,8 @@ def main() -> None:
 
         'Displacement shell': The "shell" of the displacement, i.e. which
         "displacement" value you're doing
-        'Slide': Which position along the face you're at if you're sliding, doesnt
-        actually equate to where but 0 is on axis and not 0 is off axis
+        'Slide': Which position along the face you're at if you're sliding,
+        doesnt actually equate to where but 0 is on axis and not 0 is off axis
         'Rotation': The rotation of the cage
         'Displacement centroid': The actual displacement of the centroid of the
         neighbouring cage
@@ -119,9 +122,9 @@ def main() -> None:
 
     """
 
-    If you want to constrain the atom positions this function will return the atom
-    positions of the cage you want to fix. It constrains the smiles string unless
-    metal_atom is not none.
+    If you want to constrain the atom positions this function will return the
+    atom positions of the cage you want to fix. It constrains the smiles
+    string unless metal_atom is not none.
 
     For this you need:
         dimer: stk.Molecule
@@ -129,9 +132,9 @@ def main() -> None:
         metal_atom: int. The atom index of the metal atom. If none then it will
         just fix the smiles string.
 
-    I'm pretty sure this only works if your molecule string contains an imine. I
-    imagine you wont use this so wont edit it to be better but lmk if you do want
-    it.
+    I'm pretty sure this only works if your molecule string contains an imine.
+    I imagine you wont use this so wont edit it to be better but lmk if you do
+    want it.
 
     """
 
@@ -143,21 +146,23 @@ def main() -> None:
     )
 
     """
-    This is the optimisation part. It will optimise the dimer using GULP, OPLS or
-    XTB.
+    This is the optimisation part. It will optimise the dimer using GULP, OPLS
+    or XTB.
 
     For this you need:
         dimer_entry: The dictionary entry of the dimer you want to optimise
         output_path: str. Folder you want the optimisation to happen in
         path: path to optimisation software i.e. GULP_PATH, SCHRODINGER_PATH,
         XTB_PATH
-        fixed_atom_set: set. The set of atoms you want to fix, assumed none if not
-        there.
+        fixed_atom_set: set. The set of atoms you want to fix, assumed none if
+        not there.
 
     """
 
     for dimer_entry in list_of_dimers:
-        XTB_optimizer = DimerOptimizer.optimise_dimer_XTB(
+        # Call this something like: xtb_optimizer, then use
+        # `xtb_optimizer.optimize()`
+        _ = DimerOptimizer.optimise_dimer_XTB(
             dimer=dimer_entry["Dimer"],
             output_dir=(
                 f"XTB_shell_{dimer_entry['Displacement shell']}_slide_"
